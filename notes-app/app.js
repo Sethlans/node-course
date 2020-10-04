3aef9e36e534f815073388df8df9a7a6a99e5782
Sethlans <developer@mattiagiacone.me>
2020-10-04 11:30:50 +0200

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const command = process.argv[2];
 
 console.log(process.argv);
+console.log(yargs.argv);
+
 if(command==='add'){
     console.log('Adding note');
 } else if (command==='remove'){
